712e1bdde36d380006a44654fc5150e6ce8c9091
haze25102583 <haze25102583@gmail.com>
2026-07-14 23:41:21 +0900

    Update keras06_행렬.py

--- keras06_행렬.py
+++ keras06_행렬.py
@@ -1,18 +1,17 @@
 import numpy as np
 
-x1 = np.array([1, 2, 3])
-print("x1 = ", x1.shape)    # x1 =  (3,)
-                            # => 스칼라 3개의 백터 묶음
+# 0차원 데이터 : 스칼라
 
-x2 = np.array([[1, 2, 3]])
-print("x2 = ", x2.shape)    # x2 =  (1, 3)
-                            # => 1행 3열
-                            
-x3 = np.array([[1, 2, 3], [4, 5, 6]])
-print("x3 = ", x3.shape)                   # x3 = (2, 3)
+# 1차원 데이터 : 벡터
+x1 = np.array([1, 2, 3])        # array() 함수의 parameter : object -> 대괄호 유의
+print(x1.shape)                 # shape 속성 : 차원과 차원의 크기를 반환
+                                # 속성이므로 함수처럼 ()가 붙지 않음
+                                # (3,) 출력
 
-x4 = np.array([[1, 2], [3, 4]])            
-print("x4 = ", x4.shape)                   # x4 = (2, 2)
+# 2차원 데이터 : 행렬, Matrix                                
+x2 = np.array([[1, 2, 3], [1, 2, 3]])
+print(x2.shape)                 # (2, 3) 출력
 
-x5 = np.array([[[[1]]], [[[2]]]])
-print("x5 = ", x5.shape)
+# 3차원 데이터 : Tensor
+x3 = np.array([[[1, 2], [3, 4], [5, 6], [7, 8]], [[1, 2], [3, 4], [5, 6], [7, 8]], [[1, 2], [3, 4], [5, 6], [7, 8]]])
+print(x3.shape)                 # (3, 4, 2) 출력 -> 면, 행, 열
