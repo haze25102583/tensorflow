dd8886286d9c38322e7fa0669b76ed8418be8625
haze25102583 <haze25102583@gmail.com>
2026-07-15 09:13:54 +0900

    Update keras07_mlp1.py

--- keras07_mlp1.py
+++ keras07_mlp1.py
@@ -6,8 +6,8 @@ from tensorflow.keras.layers import Dense
 import numpy as np
 
 # 1. 데이터 전처리
-# x은 [1,7], [2, 8], [3, 9], [4, 10], [5, 11], [6, 12]
-# y은 [1, 2, 3, 4, 5, 6]
+# x는 [1,7], [2, 8], [3, 9], [4, 10], [5, 11], [6, 12]
+# y는 [1, 2, 3, 4, 5, 6]
 x = np.array([[1, 7], [2, 8], [3, 9], [4, 10], [5, 11], [6, 12]])       # array() 함수의 매개변수 type : 객체
                                                                         # 여러 개의 백터(1차원) -> []로 묶어 매트릭스(2차원) '객체'로
                                                                         
@@ -21,7 +21,7 @@ exit()                                              # 프로세스 종료
 
 # 2. 모델 구성
 model = Sequential()
-model.add(Dense(100, input_shape=(2, )))            # input_shape는 매개변수 이므로 input_shape=(n차원 데이터) 형식으로 지정
+model.add(Dense(100, input_shape=(2, )))            # input_shape는 매개변수의 키워드인자가 들어가야하므로 input_shape=(n차원 데이터) 형식으로 지정
 model.add(Dense(300))
 model.add(Dense(500))
 model.add(Dense(700))
