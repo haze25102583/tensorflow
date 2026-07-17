b8ce95e892c91d2a4cead49ef6e7f2b7c18b9327
haze25102583 <haze25102583@gmail.com>
2026-07-17 16:13:08 +0900

    Add files via upload

---
+++ keras14_EarlyStopping2_diabetes.py
@@ -0,0 +1,73 @@
+# 11_2을 es 적용해서 성능 향상
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.callbacks import EarlyStopping
+
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score, root_mean_squared_error, mean_squared_error
+from sklearn.datasets import load_diabetes                                # 교육용 데이터 제공
+
+# 1. 데이터
+datasets = load_diabetes()
+# print(datasets)
+# print(datasets.DESCR)                             # describe : 조사하다
+# # dataset : 행
+# # attribute : 열
+print(datasets.feature_names)
+# ['age', 'sex', 'bmi', 'bp', 's1', 's2', 's3', 's4', 's5', 's6']
+
+# exit()
+# x, y의 데이터가 분리
+x = datasets.data
+y = datasets.target
+# print(x)
+# print(y)
+
+print(x.shape, y.shape)                 # (442, 10) (442,)
+
+x_train, x_test, y_train, y_test = train_test_split(x, y,
+                                                    train_size=0.75,        # default
+                                                    shuffle=True,            # default
+                                                    random_state=100,
+                                                    )
+
+print(x_train.shape, x_test.shape)  # (331, 10) (111, 10)
+print(y_train.shape, y_test.shape)  # (331,) (111,)
+# exit()
+
+
+
+# 2. 모델구성
+model = Sequential()
+model.add(Dense(10, activation='relu',input_shape=(10,)))
+model.add(Dense(28, activation='relu'))
+model.add(Dense(50, activation='relu'))
+model.add(Dense(19, activation='relu'))
+model.add(Dense(1, activation='linear'))
+
+
+# 3. 컴파일, 훈련
+model.compile(loss="mse", optimizer="adam")
+
+es = EarlyStopping(
+    monitor='val_loss', 
+    mode = 'min',
+    patience=100,                # 최소값이 갱신되지 않는 횟수
+    restore_best_weights=True,
+    )
+
+model.fit(x_train, y_train, epochs=128, batch_size=64,
+          validation_split=0.2,
+          callbacks = [es])                   # batch_size default값 : 32
+
+
+
+# 4. 평가, 예측
+loss = model.evaluate(x_test, y_test)
+print("loss = ", loss)                      # loss =  2601.837890625
+y_predict = model.predict(x_test)
+
+r2 = r2_score(y_test, y_predict)
+print("r2 score : ", r2)                    # r2 score :  0.5394820038443388
+
+# R2 Score = 0.6 이상 ~ 0.9
