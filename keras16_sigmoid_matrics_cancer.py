5736ca8890b38829366b77d3924b61d6ab01d505
haze25102583 <haze25102583@gmail.com>
2026-07-20 09:13:35 +0900

    Update keras16_sigmoid_matrics_cancer.py

--- keras16_sigmoid_matrics_cancer.py
+++ keras16_sigmoid_matrics_cancer.py
@@ -4,7 +4,7 @@ from tensorflow.keras.layers import Dense
 from tensorflow.keras.callbacks import EarlyStopping
 
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import r2_score, root_mean_squared_error, mean_squared_error
+from sklearn.metrics import accuracy_score, mean_squared_error
 from sklearn.datasets import load_breast_cancer                                # 교육용 데이터 제공
 
 import numpy as np
@@ -24,47 +24,6 @@ y = datasets.target
 print(x.shape, y.shape)                 # (569, 30) (569,)
 print(np.unique(y, return_counts=True))
 
-
-
-x_train, x_test, y_train, y_test = train_test_split(x, y,
-                                                    train_size=0.75,        # default
-                                                    shuffle=True,            # default
-                                                    random_state=100,
-                                                    )
-
-print(x_train.shape, x_test.shape)  # (426, 30) (143, 30)
-print(y_train.shape, y_test.shape)  # (426,) (143,)
-
-
-
-# 2. 모델구성
-model = Sequential()
-model.add(Dense(10, input_shape=(30,)))
-model.add(Dense(28))
-model.add(Dense(50))
-model.add(Dense(19))
-model.add(Dense(1))
-
-
-# 3. 컴파일, 훈련
-model.compile(loss="mse", optimizer="adam")
-start_time = time.time()
-model.fit(x_train, y_train, epochs=128, batch_size=64, verbose=3)                   # batch_size default값 : 32
-end_time = time.time()
-
-
-# 4. 평가, 예측
-loss = model.evaluate(x_test, y_test)
-print("loss = ", loss)                      # loss =  2601.837890625
-y_predict = model.predict(x_test)
-
-r2 = r2_score(y_test, y_predict)
-print("r2 score : ", r2)                    # r2 score :  0.5394820038443388
-
-print("걸린시간 : ", round(end_time-start_time, 2), "초")
-
-
-print(x.shape, y.shape)                 # (442, 10) (442,)
 
 x_train, x_test, y_train, y_test = train_test_split(x, y,
                                                     train_size=0.75,        # default
@@ -98,7 +57,7 @@ es = EarlyStopping(
     )
 
 start_time = time.time()
-model.fit(x_train, y_train, epochs=128, batch_size=64,
+model.fit(x_train, y_train, epochs=128, batch_size=64, verbose=3,
           validation_split=0.2,
           callbacks = [es])                   
 end_time = time.time()
@@ -113,6 +72,5 @@ y_predict = np.round(y_predict)
 
 # print("x_test의 예측값 : ", y_predict)
 print("걸린 시간 : ", round(end_time-start_time, 2), "초")
-from sklearn.metrics import accuracy_score
 acc_score = accuracy_score(y_test, y_predict)
 print('accuracy_score : ', acc_score)
